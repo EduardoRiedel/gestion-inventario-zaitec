2249ed71a4138e162b695fcded8d28606b6008ae
IvanGarcia-Medac <igf0015@alu.medac.es>
2026-03-26 14:19:55 +0100

    comentario

--- gestion_inventario_zaitec/lib/main.dart
+++ gestion_inventario_zaitec/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:gestion_inventario_zaitec/firebase/firebaseManager.dart';
 import 'package:gestion_inventario_zaitec/firebase/firebase_options.dart';
 import 'package:gestion_inventario_zaitec/pages/login_page.dart';
-
+ // principal
 Future<void> main() async {
   runApp(const MyApp());
 
