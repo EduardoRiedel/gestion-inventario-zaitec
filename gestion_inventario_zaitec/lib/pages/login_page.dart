2d7261eee833bee405192d2e9a4a32da19f5dfc8
IvanGarcia-Medac <igf0015@alu.medac.es>
2026-03-26 13:14:41 +0100

    Funcionalidades adicionales login contraseña

--- gestion_inventario_zaitec/lib/pages/login_page.dart
+++ gestion_inventario_zaitec/lib/pages/login_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'registration__page.dart';
 import '../styles.dart';
 import 'citas_page.dart';
-import 'package:gestion_inventario_zaitec/Firebase/FirebaseManager.dart';
+import 'package:gestion_inventario_zaitec/firebase/firebaseManager.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -14,6 +14,9 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+
+  bool _oscurecerPassword = true; // Por defecto la contraseña está oculta
+
   bool _obscurePassword = true;
 
   @override
@@ -23,29 +26,19 @@ class _LoginPageState extends State<LoginPage> {
     super.dispose();
   }
 
-  void _login() {
-    // Add login logic here
-    final email = _emailController.text;
-    final password = _passwordController.text;
-    
-    if (email.isNotEmpty && password.isNotEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Login successful')),
-      );
-    }
-  }
-
- @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea( // <--- ESTO evita que el texto se esconda arriba del todo
-        child: Center( // <--- ESTO obliga a centrarlo
-          child: SingleChildScrollView( // <--- ESTO permite que se vea con el teclado abierto
+      body: SafeArea(
+        // <--- ESTO evita que el texto se esconda arriba del todo
+        child: Center(
+          // <--- ESTO obliga a centrarlo
+          child: SingleChildScrollView(
+            // <--- ESTO permite que se vea con el teclado abierto
             padding: const EdgeInsets.all(32.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-
                 // PRUEBA: Título con color rojo fuerte para que lo veas sí o sí
                 Text('ZAITEC PELUQUEROS', style: AppStyles.tituloPrincipal),
                 const SizedBox(height: 10),
@@ -53,24 +46,45 @@ class _LoginPageState extends State<LoginPage> {
                 Text('Gestión de Citas', style: AppStyles.subtitulo),
 
                 const SizedBox(height: 50),
-                
+
                 // Campos de texto
                 TextField(
                   controller: _emailController,
-                  decoration: AppStyles.fieldDecoration('Email', Icons.email_outlined), // Icono más fino
+                  decoration: AppStyles.fieldDecoration(
+                    'Email',
+                    Icons.email_outlined,
+                  ), // Icono más fino
                 ),
 
                 const SizedBox(height: 16),
 
                 TextField(
                   controller: _passwordController,
-                  obscureText: _obscurePassword,
-                  // Usamos Icons.lock_outline para el look minimalista
-                  decoration: AppStyles.fieldDecoration('Password', Icons.lock_outline), 
+                  obscureText: _obscurePassword, // Tu variable bool
+                  decoration:
+                      AppStyles.fieldDecoration(
+                        'Password',
+                        Icons.lock_outline,
+                      ).copyWith(
+                        // <--- Esto mantiene tus estilos y añade lo nuevo
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _obscurePassword
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                            color: Colors.grey, // O el color que prefieras
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _obscurePassword = !_obscurePassword;
+                            });
+                          },
+                        ),
+                      ),
                 ),
 
                 const SizedBox(height: 30),
-                
+
                 ElevatedButton(
                   style: AppStyles.botonPrincipal,
                   onPressed: () {
@@ -83,13 +97,13 @@ class _LoginPageState extends State<LoginPage> {
                     // ESTO es lo que hace el salto de página:
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const AgendaPage()),
+                      MaterialPageRoute(
+                        builder: (context) => const AgendaPage(),
+                      ),
                     );
                   },
                   child: const Text('INICIAR SESIÓN'),
                 ),
-                
-                
 
                 const SizedBox(height: 20),
 
@@ -100,7 +114,9 @@ class _LoginPageState extends State<LoginPage> {
                     // ESTO es lo que hace el salto de página:
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const RegisterPage()),
+                      MaterialPageRoute(
+                        builder: (context) => const RegisterPage(),
+                      ),
                     );
                   },
                   child: const Text('¿No tienes cuenta? Regístrate aquí'),
--- gestion_inventario_zaitec/lib/pages/registration__page.dart
+++ gestion_inventario_zaitec/lib/pages/registration__page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gestion_inventario_zaitec/pages/login_page.dart';
 import '../styles.dart';
-import 'package:gestion_inventario_zaitec/Firebase/FirebaseManager.dart';
+import 'package:gestion_inventario_zaitec/firebase/firebaseManager.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -13,7 +13,9 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
   bool _obscurePassword = true;
+  bool _obscureConfirm = true;
 
   @override
   void dispose() {
@@ -22,18 +24,19 @@ class _RegisterPageState extends State<RegisterPage> {
     super.dispose();
   }
 
-
- @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea( // <--- ESTO evita que el texto se esconda arriba del todo
-        child: Center( // <--- ESTO obliga a centrarlo
-          child: SingleChildScrollView( // <--- ESTO permite que se vea con el teclado abierto
+      body: SafeArea(
+        // <--- ESTO evita que el texto se esconda arriba del todo
+        child: Center(
+          // <--- ESTO obliga a centrarlo
+          child: SingleChildScrollView(
+            // <--- ESTO permite que se vea con el teclado abierto
             padding: const EdgeInsets.all(32.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-
                 // PRUEBA: Título con color rojo fuerte para que lo veas sí o sí
                 Text('ZAITEC PELUQUEROS', style: AppStyles.tituloPrincipal),
                 const SizedBox(height: 10),
@@ -41,11 +44,14 @@ class _RegisterPageState extends State<RegisterPage> {
                 Text('Gestión de Citas', style: AppStyles.subtitulo),
 
                 const SizedBox(height: 50),
-                
+
                 // Campos de texto
                 TextField(
                   controller: _emailController,
-                  decoration: AppStyles.fieldDecoration('Email', Icons.email_outlined), // Icono más fino
+                  decoration: AppStyles.fieldDecoration(
+                    'Email',
+                    Icons.email_outlined,
+                  ), // Icono más fino
                 ),
 
                 const SizedBox(height: 16),
@@ -53,28 +59,59 @@ class _RegisterPageState extends State<RegisterPage> {
                 TextField(
                   controller: _passwordController,
                   obscureText: _obscurePassword,
-                  // Usamos Icons.lock_outline para el look minimalista
-                  decoration: AppStyles.fieldDecoration('Password', Icons.lock_outline), 
+                  decoration:
+                      AppStyles.fieldDecoration(
+                        'Contraseña',
+                        Icons.lock_outline,
+                      ).copyWith(
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _obscurePassword
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                          onPressed: () => setState(
+                            () => _obscurePassword = !_obscurePassword,
+                          ),
+                        ),
+                      ),
+                ),
+                // CAMPO DE CONFIRMAR CONTRASEÑA
+                TextField(
+                  controller: _confirmPasswordController,
+                  obscureText: _obscureConfirm,
+                  decoration:
+                      AppStyles.fieldDecoration(
+                        'Confirmar Contraseña',
+                        Icons.lock_reset,
+                      ).copyWith(
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _obscureConfirm
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                          onPressed: () => setState(
+                            () => _obscureConfirm = !_obscureConfirm,
+                          ),
+                        ),
+                      ),
                 ),
 
                 const SizedBox(height: 30),
                 ElevatedButton(
                   style: AppStyles.botonPrincipal,
-                  onPressed: () {
-
-                    final email = _emailController.text;
-                    final password = _passwordController.text;
-
-                    // Llamar al método de registro del FirebaseManager
-                    FirebaseManager().registrarUsuario(email, password);
-
-                    Navigator.push(
+                  onPressed: () async {
+                    FirebaseManager().registrarUsuario(
                       context,
-                      MaterialPageRoute(builder: (context) => const LoginPage()),
+                      _emailController.text,
+                      _passwordController.text,
+                      _confirmPasswordController.text,
                     );
+
+                    // llamo al cambio de ventana en el propio metodo de firebaseManager
                   },
                   child: const Text('Crear Cuenta'),
-                  
                 ),
               ],
             ),
