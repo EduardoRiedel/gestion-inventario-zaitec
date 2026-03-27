d4193cb74fc4c3187d81aab3934c0c8264ad95b5
IvanGarcia-Medac <igf0015@alu.medac.es>
2026-03-27 09:33:08 +0100

    Cambio para subir main completo y no fastidiar el proyecto

--- gestion_inventario_zaitec/lib/main.dart
+++ gestion_inventario_zaitec/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:gestion_inventario_zaitec/Firebase/FirebaseManager.dart';
 import 'package:gestion_inventario_zaitec/Firebase/firebase_options.dart';
 import 'package:gestion_inventario_zaitec/pages/login_page.dart';
- // principal
+ // Principal
 Future<void> main() async {
   runApp(const MyApp());
 
